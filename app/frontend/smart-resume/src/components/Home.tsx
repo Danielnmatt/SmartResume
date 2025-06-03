a44c2c19ec2f881ea202908f9f5fb707e14be5a6
Danielnmatt <danielnmatt@gmail.com>
2025-06-03 00:53:40 -0400

    Main Page layout

--- app/frontend/smart-resume/src/components/Home.tsx
+++ app/frontend/smart-resume/src/components/Home.tsx
@@ -1,14 +1,16 @@
 import "../styles/tailwind.css";
+import Test from "./test";
 const Home = () => {
-    return (
-        <div className={"flex flex-col h-screen w-screen bg-[#010104]"}>
-            <div className="flex bg-secondary h-1/12 w-full justify-center">
-                <h1 className={"h-min text-primary text-6xl"}>SMART RESUME</h1>
+	return (
+		<div className={"flex flex-col h-screen w-screen bg-[#010104]"}>
+			<div className="flex bg-secondary h-1/12 w-full justify-center">
+				<h1 className={"h-min text-primary text-6xl"}>SMART RESUME</h1>
+			</div>
+			<div className="m-auto w-1/4 h-1/4">
+                <Test/>
             </div>
-            <div className="m-auto h-1/4 w-1/4 bg-amber-700">
-            </div>
-        </div>
-    )
-}
+		</div>
+	);
+};
 
-export default Home
+export default Home;
