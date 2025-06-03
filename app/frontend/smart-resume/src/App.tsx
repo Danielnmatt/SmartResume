2c31fefd0791d3931cc74831b01947d53c87f549
Danielnmatt <danielnmatt@gmail.com>
2025-06-02 23:29:23 -0400

    Working on main page

--- app/frontend/smart-resume/src/App.tsx
+++ app/frontend/smart-resume/src/App.tsx
@@ -1,12 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WelcomePage from "./components/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router";
+import LoginPage from "./components/LoginPage";
+import SignUpPage from "./components/SignUpPage"
+import Main from "./components/Home";
 import './styles/App.css';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<WelcomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/" element={<Main />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ app/frontend/smart-resume/src/components/SignUpPage.tsx
@@ -0,0 +1,81 @@
+import "../styles/tailwind.css";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+//TODO: Handle Sign Up logic, Email verification upon sign up
+const SignUpPage = () => {
+	return (
+		<div className="flex h-screen w-screen">
+			<div className="m-auto h-5/6 w-7/24 flex flex-col items-center rounded-xl">
+				<h1 className="text-5xl mb-5">Smart Resume</h1>
+				{<PageView />}
+			</div>
+		</div>
+	);
+};
+
+const PageView = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const navigate = useNavigate();
+
+	const handleSignUp = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		navigate("/");
+	};
+
+	return (
+		<div className="flex flex-col items-center self-center justify-self-center h-4/5 w-4/5">
+			<h1 className="text-4xl">Sign up</h1>
+			<div className="w-9/10 mt-12 h-4/5">
+				<form className="flex flex-col">
+					<label htmlFor="email">Email</label>
+					<input
+						autoComplete="off"
+						id="email"
+						type="text"
+						placeholder="frjohncena@gmail.com"
+						className="mb-6 p-2 rounded-md w-full border-2"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<label htmlFor="password">Password</label>
+					<input
+						autoComplete="off"
+						id="password"
+						type="password"
+						placeholder="ucanTseeMe!"
+						className="mb-6 p-2 rounded-md w-full border-2"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<label htmlFor="confirmPassword">Confirm Password</label>
+					<input
+						autoComplete="off"
+						id="confirmPassword"
+						type="password"
+						placeholder="ucanTseeMe!"
+						className="mb-4 p-2 rounded-md w-full border-2"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					/>
+					<button
+						type="submit"
+						onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleSignUp(e)}
+						className="self-center w-11/12 px-8 py-4 font-bold bg-[rgb(0,0,0)] text-[blueviolet] cursor-pointer rounded-full border-b-[2px_solid_blueviolet] border-r-[2px_solid_blueviolet] border-t-[2px_solid_white] border-l-[2px_solid_white] duration-1000 [transition-property:border-top,_border-left,_border-bottom,_border-right,_box-shadow] hover:border-t-[2px_solid_blueviolet] hover:border-l-[2px_solid_blueviolet] hover:border-b-[2px_solid_rgb(238,_103,_238)] hover:border-r-[2px_solid_rgb(238,_103,_238)] hover:[box-shadow:rgba(240,_46,_170,_0.4)_5px_5px,_rgba(240,_46,_170,_0.3)_10px_10px,_rgba(240,_46,_170,_0.2)_15px_15px] mb-8">
+						Sign Up
+					</button>
+					<button
+						type="button"
+						className="w-fit self-center hover:underline hover:cursor-pointer"
+						onClick={() => navigate("/login")}>
+						{"< Back To Login"}
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default SignUpPage;
